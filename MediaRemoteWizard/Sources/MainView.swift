281caabc0bb256940a20964a0260cee88af847de
Mx-Iris <mxiris@126.com>
2025-06-16 00:10:42 +0800

    Support hide main window on launch

--- MediaRemoteWizard/Sources/MainView.swift
+++ MediaRemoteWizard/Sources/MainView.swift
@@ -2,14 +2,18 @@ import SwiftUI
 import LaunchAtLogin
 
 struct MainView: View {
-    var viewModel: MainViewModel = .init()
+    @EnvironmentObject
+    private var client: MediaRemoteWizardClient
 
     @State
-    var error: String?
+    private var error: String?
 
     @State
-    var isPresentedError: Bool = false
+    private var isPresentedError: Bool = false
 
+    @AppStorage("showMainWindowOnLaunch")
+    private var showMainWindowOnLaunch: Bool = true
+    
     var body: some View {
         Image(nsImage: NSApplication.shared.applicationIconImage)
             .resizable()
@@ -25,7 +29,7 @@ struct MainView: View {
                     Button {
                         Task {
                             do {
-                                try await viewModel.installHelper()
+                                try await client.installHelper()
                             } catch {
                                 print(error)
                                 self.error = "\(error)"
@@ -33,7 +37,7 @@ struct MainView: View {
                             }
                         }
                     } label: {
-                        if viewModel.isHelperConnected {
+                        if client.isHelperConnected {
                             Image(systemName: "checkmark.circle.fill")
                                 .foregroundColor(.green)
                                 .font(.title2)
@@ -51,6 +55,12 @@ struct MainView: View {
                         Text("Launch at login")
                     }
                 }
+                
+                HStack {
+                    Toggle(isOn: $showMainWindowOnLaunch) {
+                        Text("Show main window")
+                    }
+                }
             }
         }
         .formStyle(.grouped)
@@ -66,4 +76,5 @@ struct MainView: View {
 @available(macOS 14.0, *)
 #Preview {
     MainView()
+        .environmentObject(MediaRemoteWizardClient())
 }
